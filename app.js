78b8e2c688b12edeb2954b3f83cdaf17baea2192
Will Carpenter <willcarpenteriii@gmail.com>
2017-11-17 14:19:16 -0700

    storing user input to stdin to db file

--- app.js
+++ app.js
@@ -1,28 +1,58 @@
+const fs = require('fs')
 const commandLineArgs = require('command-line-args')
 
 const optionsOption = [
 	{ name: 'name', type: String },
 	{ name: 'order', type: String },
-	{ name: 'payment', type: Number }
+	{ name: 'payment', type: Number },
+	{ name: 'exit', type: Boolean}
 ]
 
 const options = commandLineArgs(optionsOption)
 
+let getJson = fs.readFileSync('db.json')
+let objData = JSON.parse(getJson)
+
+const save = newData => {
+	const toString = JSON.stringify(newData)
+	fs.writeFileSync('db.json', toString)
+}
+
+
 if(options.name) {
+	objData.name = options.name
 
 	console.log(`Hello, ${options.name}, we are serving cake, pizza, and salad`)
 
+	save(objData)
+
 } else if(options.order) {
+
+	objData.order = options.order
 
 	console.log(`Ok, that costs $25. You must pay with`)
 
+	save(objData)
+
 } else if(options.payment) {
 
-	console.log(`Your change is nada, now leave.`)
+	objData.payment = options.payment
+
+	console.log(`Your change is ${options.payment - 25} dollars, now leave.`)
+
+	save(objData)
 
 } else if(options.exit) {
 
-	console.log('Get the fuck out!')
+	console.log(objData)
+
+	console.log('Leave!!')
+
+	objData.name = ''
+	objData.order = ''
+	objData.payment = ''
+
+	save(objData)
 
 } else {
 
